411618f940d073d173add770dfb7f23aacad0b8e
Khaled Adad <47358621+khaloodi@users.noreply.github.com>
2019-12-20 10:01:31 -0600

    Update names.py

--- names/names.py
+++ names/names.py
@@ -3,19 +3,19 @@ from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
-# f = open('names_1.txt', 'r')
-# names_1 = f.read().split("\n")  # List containing 10000 names
-# f.close()
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
 
-# f = open('names_2.txt', 'r')
-# names_2 = f.read().split("\n")  # List containing 10000 names
-# f.close()
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
 
-# duplicates = []
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
+duplicates = []
+for name_1 in names_1:
+    for name_2 in names_2:
+        if name_1 == name_2:
+            duplicates.append(name_1)
 
 names = BinarySearchTree(names_1[0])
 for i in names_1:
